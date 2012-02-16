2e88c47f98d2da8d682f88b2049ca196b2fbfa54
apath <ashcroftstraaf@gmail.com>
2012-02-16 19:30:53 +0100

    mis à jour

--- FavorisLink/compile_debug.bat
+++ FavorisLink/compile_debug.bat
@@ -1,2 +1,2 @@
-c:\mingw\bin\gcc -g -ansi -Wall -Wextra src\main.c src\flk_save.c -o bin\debug\FavorisLink.exe
+c:\mingw\bin\gcc -g -ansi -Wall -Wextra src\main.c src\flk_manip.c -o bin\debug\FavorisLink.exe
 @pause
