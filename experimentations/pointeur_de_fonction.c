2265c58dbb4f35f64b4d43b0ee1940663ed22af3
cdeb <spycom@orange.fr>
2012-02-18 12:56:39 +0100

    pointeur fonctions

---
+++ experimentations/pointeur_de_fonction.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void mafonction(int a){
+    printf("%d\n",a);
+}
+void dixfois(void(*pointeur)(int),int a){
+    int i;
+    for(i=0;i<10;i++)
+        pointeur(a);
+}
+
+int main ( int argc, char** argv ){
+
+    void (*pointeur) (inta);
+    pointeur=mafonction;
+
+    pointeur(7);
+
+    mafonction(5);
+
+    dixfois(mafonction,5);
+
+    return 0;
+}
