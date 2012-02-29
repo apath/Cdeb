d6ae10084219de3cc350ee9ac36719163b9819a9
apath <ashcroftstraaf@gmail.com>
2012-02-29 22:19:14 +0100

    maj

--- FavorisLink/compile_debug.bat
+++ FavorisLink/compile_debug.bat
@@ -1,2 +1,2 @@
-c:\mingw\bin\gcc -g -ansi -Wall -Wextra src\main.c src\flk_manip.c src\flk_fichier.c -o bin\debug\FavorisLink.exe
+c:\mingw\bin\gcc -g -ansi -Wall -Wextra src\main.c src\flk_manip.c src\flk_fichier.c src\flk_console.c -o bin\debug\FavorisLink.exe
 @pause
