825746c117c3bc201b835003f0b88d60c29861f3
apath <ashcroftstraaf@gmail.com>
2012-02-29 23:00:27 +0100

    ...

--- FavorisLink/compile_release.bat
+++ FavorisLink/compile_release.bat
@@ -1,2 +1,2 @@
-c:\mingw\bin\gcc -ansi -Wall -Wextra -s -O2 src\main.c src\flk_manip.c src\flk_console.c -o bin\release\FavorisLink.exe
+c:\mingw\bin\gcc -ansi -Wall -Wextra -s -O2 src\main.c src\flk_manip.c src\flk_fichier.c src\flk_console.c -o bin\release\FavorisLink.exe
 @pause
