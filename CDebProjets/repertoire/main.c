718434eab8f7672beca1cfa632de3ae2892d403f
apath <ashcroftstraaf@gmail.com>
2012-03-27 19:28:15 +0200

    correction

--- CDebProjets/repertoire/main.c
+++ CDebProjets/repertoire/main.c
@@ -8,14 +8,11 @@ int main(void)
     struct Fiche contact[10];
     initialise_fiche(contact,10);
 
-    contact[0].id=1;
-    contact[0].nom=longueur_chaine("Dupont");
-    contact[0].date_naissance=ret_naissance(30,1,1980);
-
+    ajouter_contact(contact,10);
     ajouter_contact(contact,10);
 
     recherche_nom(contact,longueur_chaine("DUPONT"),10);
-    
+
     affiche_tout(contact,10);
     save_fiche(contact,10,"fichier.txt");
 
