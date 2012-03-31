e34525090bc16beccc4e1bebf25fc58c9f2fce85
apath <ashcroftstraaf@gmail.com>
2012-03-31 21:34:48 +0200

    main pour le test de chargement des données

--- CDebProjets/repertoire/main.c
+++ CDebProjets/repertoire/main.c
@@ -8,14 +8,14 @@ int main(void)
     initialise_fiche(contact,10);
     load_fiche(contact,10,"fichier.txt");
 
-    ajouter_contact(contact,10);
+    /*ajouter_contact(contact,10);
     ajouter_contact(contact,10);
 
     recherche_nom(contact,"DUPONT",10);
     save_fiche(contact,10,"fichier.txt");
     affiche_tout(contact,10);
 
-    supprime_tout(contact,10);
+    supprime_tout(contact,10);*/
     affiche_tout(contact,10);
 
     return 0;
