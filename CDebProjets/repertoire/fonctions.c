3b4ca385c5e56244d1ca5afc31aae171e37fe9b1
cdeb <spycom@orange.fr>
2012-04-06 22:59:55 +0200

    modif load

--- CDebProjets/repertoire/fonctions.c
+++ CDebProjets/repertoire/fonctions.c
@@ -4,8 +4,9 @@
 #include <ctype.h>
 #include "fonctions.h"
 
-  /*recherche par nom*/
-int recherche_nom(struct Fiche *contact,const char *nom_rechercher,int capacite){
+/*recherche par nom*/
+int recherche_nom(struct Fiche *contact,const char *nom_rechercher,int capacite)
+{
     char *buff_nom;
     int i,l;
     l=strlen(nom_rechercher);
@@ -14,17 +15,23 @@ int recherche_nom(struct Fiche *contact,const char *nom_rechercher,int capacite)
                                           caractère \0 */
     if(!buff_nom) exit(1);
     strcpy(buff_nom,nom_rechercher);
-    for(i=0;i<l;i++){
-        if(i==0){
+    for(i=0; i<l; i++)
+    {
+        if(i==0)
+        {
             buff_nom[i]=toupper(buff_nom[i]);
         }
-        else{
+        else
+        {
             buff_nom[i]=tolower(buff_nom[i]);
         }
     }
-    for(i=0;i<capacite;i++){
-        if(contact[i].id!=0){
-            if(!strcmp(contact[i].nom,buff_nom)) {
+    for(i=0; i<capacite; i++)
+    {
+        if(contact[i].id!=0)
+        {
+            if(!strcmp(contact[i].nom,buff_nom))
+            {
                 affiche_id(contact,i);
                 return 0; /* le nom est trouvé */
             }
@@ -35,17 +42,20 @@ int recherche_nom(struct Fiche *contact,const char *nom_rechercher,int capacite)
 }
 
 
-  /*supprime toutes les fiches existantes par recherche d'ID*/
-void supprime_tout(struct Fiche *contact,int capacite){
+/*supprime toutes les fiches existantes par recherche d'ID*/
+void supprime_tout(struct Fiche *contact,int capacite)
+{
     int i;
-    for(i=0;i<capacite;i++){
+    for(i=0; i<capacite; i++)
+    {
         if(contact[i].id!=0) supp_contact(contact,i+1);
     }
 }
 
-  /*Ajout de la fonction supp_contact.
-   *int i correspond a l'id de la fiche contact à supprimer. */
-void supp_contact(struct Fiche *contact,int i){
+/*Ajout de la fonction supp_contact.
+ *int i correspond a l'id de la fiche contact à supprimer. */
+void supp_contact(struct Fiche *contact,int i)
+{
     i--;
     contact[i].id=0;
     free(contact[i].nom),contact[i].nom=NULL;
@@ -57,33 +67,40 @@ void supp_contact(struct Fiche *contact,int i){
     free(contact[i].adresse),contact[i].adresse=NULL;
 }
 
-  /* get_str remplace le scanf, on lui passe un buffer en paramètre et la taille max
-   * de celui-ci (max-1 pour le 0 de fin de chaîne) et la fonction utilise getchar()
-   * pour remplir le buffer puis termine la châine par zéro et la retourne */
-char *get_str(char *s,int max){
-    int i; char c;
-    for(i=0;(c=getchar())!='\n'&&i<max-1;i++) s[i]=c;
+/* get_str remplace le scanf, on lui passe un buffer en paramètre et la taille max
+ * de celui-ci (max-1 pour le 0 de fin de chaîne) et la fonction utilise getchar()
+ * pour remplir le buffer puis termine la châine par zéro et la retourne */
+char *get_str(char *s,int max)
+{
+    int i;
+    char c;
+    for(i=0; (c=getchar())!='\n'&&i<max-1; i++) s[i]=c;
     s[i]='\0';
     return s;
 }
 
-void ajouter_contact(struct Fiche *contact, int capacite){
+void ajouter_contact(struct Fiche *contact, int capacite)
+{
     char buff[256];
     int i,j;
 
-    for(i=0;i<capacite;i++)
-        if(contact[i].id==0){
+    for(i=0; i<capacite; i++)
+        if(contact[i].id==0)
+        {
             contact[i].id=i+1;
             break;
         }
     printf("Nom: ");
     contact[i].nom=longueur_chaine(get_str(buff,256));
 
-    for(j=0;contact[i].nom[j];j++){
-        if(j==0){
+    for(j=0; contact[i].nom[j]; j++)
+    {
+        if(j==0)
+        {
             contact[i].nom[j]=toupper(contact[i].nom[j]);
         }
-        else{
+        else
+        {
             contact[i].nom[j]=tolower(contact[i].nom[j]);
         }
     }
@@ -92,8 +109,8 @@ void ajouter_contact(struct Fiche *contact, int capacite){
     printf("Sexe: ");
     contact[i].sexe=longueur_chaine(get_str(buff,256));
     printf("Date de naissance: ");
-      /*pour la date attendre que yrnas ait fait la fonction de parsing "0/0/0" */
-      /*contact[i].date_naissance=x(get_str(buff,256));*/
+    /*pour la date attendre que yrnas ait fait la fonction de parsing "0/0/0" */
+    /*contact[i].date_naissance=x(get_str(buff,256));*/
     printf("Email: ");
     contact[i].email=longueur_chaine(get_str(buff,256));
     printf("Telephone fixe: ");
@@ -104,10 +121,12 @@ void ajouter_contact(struct Fiche *contact, int capacite){
     contact[i].adresse=longueur_chaine(get_str(buff,256));
 }
 
-void initialise_fiche(struct Fiche *contact,int capacite){
+void initialise_fiche(struct Fiche *contact,int capacite)
+{
     int i;
 
-    for(i=0;i<capacite;i++){
+    for(i=0; i<capacite; i++)
+    {
         contact[i].id=0;
         contact[i].nom=NULL;
         contact[i].prenom=NULL;
@@ -119,23 +138,27 @@ void initialise_fiche(struct Fiche *contact,int capacite){
     }
 }
 
-  /*affiche toute les fiches existantes par recherche d'ID*/
-void affiche_tout(struct Fiche *contact,int capacite){
+/*affiche toute les fiches existantes par recherche d'ID*/
+void affiche_tout(struct Fiche *contact,int capacite)
+{
     int i;
-    for(i=0;i<capacite;i++){
+    for(i=0; i<capacite; i++)
+    {
         if(contact[i].id!=0) affiche_id(contact,i+1);
     }
 }
 
-  /*affiche une fiche demande par son id*/
-void affiche_id(struct Fiche *contact,int i){
+/*affiche une fiche demande par son id*/
+void affiche_id(struct Fiche *contact,int i)
+{
     i--;
     printf("---------------------------\n");
     printf("Identifiant: %d\n", contact[i].id);
     printf("Nom: %s\n", contact[i].nom);
     printf("Prenom: %s\n", contact[i].prenom);
     printf("Sexe: %s\n", contact[i].sexe);
-    printf("Date de naissance: "); affiche_naissance(contact[i].date_naissance);
+    printf("Date de naissance: ");
+    affiche_naissance(contact[i].date_naissance);
     printf("Email: %s\n", contact[i].email);
     printf("Telephone fixe: %s\n", contact[i].numfix);
     printf("Telephone mobile: %s\n", contact[i].nummobil);
@@ -143,34 +166,42 @@ void affiche_id(struct Fiche *contact,int i){
     printf("---------------------------\n");
 }
 
-int recherche_id(struct Fiche *contact,int id_rechercher,int capacite){
+int recherche_id(struct Fiche *contact,int id_rechercher,int capacite)
+{
     int i;
-    for(i=0;i<capacite;i++){
+    for(i=0; i<capacite; i++)
+    {
         if(contact[i].id==(id_rechercher-1)) return 0; /* l'id est trouvé */
     }
     return 1; /* l'id n'est pas trouvé */
 }
 
-char *longueur_chaine(char *chaine){ /* la fonction retourne un pointeur */
+char *longueur_chaine(char *chaine)  /* la fonction retourne un pointeur */
+{
     char *pointeur= NULL,vide[5]="vide";
     int n;
     n=strlen(chaine);
-    if(n!=0){
+    if(n!=0)
+    {
         pointeur = malloc(sizeof(char)*(n+1));
         if(pointeur) strcpy(pointeur,chaine);
-    } else {
+    }
+    else
+    {
         pointeur = malloc(sizeof(char)*5);
         if(pointeur) strcpy(pointeur,vide);
     }
-    if (pointeur == NULL) {   /* On vérifie si l'allocation a marché ou pas */
+    if (pointeur == NULL)     /* On vérifie si l'allocation a marché ou pas */
+    {
         exit(1); /* On arrête tout */
     }
     return pointeur;
 }
 
-  /* ret_naissance retourne une date dans un time_t
-   * on affiche le time_t avec la fonction ctime() */
-time_t ret_naissance(int j,int m,int a){
+/* ret_naissance retourne une date dans un time_t
+ * on affiche le time_t avec la fonction ctime() */
+time_t ret_naissance(int j,int m,int a)
+{
     time_t date_naissance;
     struct tm *dn;
     date_naissance = time(NULL);
@@ -182,15 +213,17 @@ time_t ret_naissance(int j,int m,int a){
     return date_naissance;
 }
 
-  /* affichage de la date de naissance en time_t sous la forme j/m/a */
-void affiche_naissance(time_t date){
+/* affichage de la date de naissance en time_t sous la forme j/m/a */
+void affiche_naissance(time_t date)
+{
     struct tm *dn;
     dn = localtime(&date);
     printf("%d/%d/%d\n",dn->tm_mday,dn->tm_mon+1,dn->tm_year+1900);
 }
 
-  /*Ajout dans un fichier texte*/
-int save_fiche(struct Fiche *contact,int capacite,const char *fichier){
+/*Ajout dans un fichier texte*/
+int save_fiche(struct Fiche *contact,int capacite,const char *fichier)
+{
     int i;
     char retour='\n',virgule=',';
     char date[9]="00/00/00";
@@ -203,9 +236,12 @@ int save_fiche(struct Fiche *contact,int capacite,const char *fichier){
                                     si on venait à ajouter à la fin du fichier la fiche vu qu'on n'utilise
                                     pas le fichier comme une base de donnée mais comme une sauvegarde :
                                     la fiche étant entièrement chargée en mémoire on utilise "w" */
-    if (FICHIER != NULL){
-        for(i=0;i<capacite;i++){
-            if(contact[i].id!=0){
+    if (FICHIER != NULL)
+    {
+        for(i=0; i<capacite; i++)
+        {
+            if(contact[i].id!=0)
+            {
                 fwrite(contact[i].nom,sizeof(char),strlen(contact[i].nom),FICHIER);
                 fwrite(&virgule,sizeof(char),1,FICHIER);
                 fwrite(contact[i].prenom,sizeof(char),strlen(contact[i].prenom),FICHIER);
@@ -231,63 +267,81 @@ int save_fiche(struct Fiche *contact,int capacite,const char *fichier){
     return 1;
 }
 
- /* Lecture du fichier .txt*/
-int load_fiche(struct Fiche *contact,int capacite,char *nom_fichier){
+/* Lecture du fichier .txt*/
+int load_fiche(struct Fiche *contact,int capacite,char *nom_fichier)
+{
     size_t i,count; /* i et count sont des compteurs en size_t car fread nous retourne
                        le nombre d'elements lu ici des char : le nombre de bytes lu */
+    char tampon[500];
     char buff[256]; /* pareil qu'avec fwrite, on utilisera un buffer ici
                        pour charger le fichier par morceaux avec fread */
-    int ctd,ctc,ctb; /* compteur de donnée,de contacts,de bytes */
+    int ct_donnee,ct_contact,ct_byte; /* compteur de donnée,de contacts,de bytes */
 
     FILE *FICHIER=NULL;
-    ctd=ctc=ctb=0; /* on met tout les compteurs à zéro */
+    ct_donnee=ct_contact=ct_byte=0; /* on met tout les compteurs à zéro */
     FICHIER=fopen(nom_fichier,"rb"); /* pas besoin de fseek on se trouve déjà au début du fichier
                                         fseek ralentit concidérablement le temps de lecture il ne
                                         faut l'utiliser que lorsqu'on ne peut pas faire autrement, ou
                                         simplement pour récupérer la taille d'un fichier */
-    if (FICHIER != NULL){
-        while((count=fread(buff,sizeof(char),256,FICHIER))){ /* tant que fread lit quelque chose */
-            for(i=0;i<count;i++){ /* on peut alors utiliser ce que fread a chargé dans buff en sachant
-                                     le nombre de char qu'il a chargé dedans */
-                if(buff[i]==','){/* si on tombe sur une virgule */
-                    switch(ctd){ /* on peut utiliser un switch pour le compteur de données nous
-                                    permettant de savoir quelle est le type de la donnée lu */
-                        case 0:printf("\n< Nom\n"); break; /* si 0 alors c'est le nom donc la chaine
-                                                            qui sera contenu dans notre tableau de char
-                                                            pourra être envoyée dans le contact actuel
-                                                            dans l'entrée "nom" de celui-ci */
-                        case 1:printf("\n< Prenom\n"); break;
-                        case 2:printf("\n< Sexe\n"); break;
-                        case 3:printf("\n< Date naissace\n"); break;
-                        case 4:printf("\n< Email\n"); break;
-                        case 5:printf("\n< Telephone fixe\n"); break;
-                        case 6:printf("\n< Telephone mobil\n"); break;
-                        case 7:printf("\n< Adresse\n"); break;
-                        default:; /* le default vide pour fermer le switch */
+    if (FICHIER != NULL)
+    {
+        while((count=fread(buff,sizeof(char),256,FICHIER)))  /* tant que fread lit quelque chose */
+        {
+            for(i=0; i<count; i++)
+            {
+                /* on peut alors utiliser ce que fread a chargé dans buff en sachant
+                                         le nombre de char qu'il a chargé dedans */
+                if(buff[i]==',') /* si on tombe sur une virgule */
+                {
+                    tampon[ct_byte+1]='\0';
+                    switch(ct_donnee)
+                    {
+                        /* on peut utiliser un switch pour le compteur de données nous
+                                        permettant de savoir quelle est le type de la donnée lu */
+                    case 0:
+                        contact[ct_contact].nom=longueur_chaine(tampon);
+
+                        break;
+                    case 1:
+                        contact[ct_contact].prenom=longueur_chaine(tampon);
+                        break;
+                    case 2:
+                        contact[ct_contact].sexe=longueur_chaine(tampon);
+                        break;
+                   /* case 3:
+                        contact[ct_contact].date_naissance=longueur_chaine(tampon);*/
+                        break;
+                    case 4:
+                        contact[ct_contact].email=longueur_chaine(tampon);
+                        break;
+                    case 5:
+                        contact[ct_contact].numfix=longueur_chaine(tampon);
+                        break;
+                    case 6:
+                        contact[ct_contact].nummobil=longueur_chaine(tampon);
+                        break;
+                    case 7:
+                        contact[ct_contact].adresse=longueur_chaine(tampon);
+                        break;
+                    default:; /* le default vide pour fermer le switch */
                     }
                     printf("une virgule! compteur de bytes (ctb) = %d,compteur de données (ctd) =%d\n\n",
-                            ctb,ctd);
-                    ctb=0; /* compteur de bytes à zéro pour la chaine suivante */
-                    ctd+=1; /* incrémente le compteur de données */
+                           ct_byte,ct_donnee);
+                    ct_byte=0; /* compteur de bytes à zéro pour la chaine suivante */
+                    ct_donnee+=1; /* incrémente le compteur de données */
                 }
-                else if(buff[i]=='\n'){ /* si on tombe sur un saut de ligne */
-                    printf("\nun saut de ligne! compteur de contacts (ctc) = %d\n",ctc);
-                    ctc+=1; /* incrémente le compteur de contacts */
-                    ctd=0; /* met le compteur de données à zéro pour la suite */
-                    ctb=0; /* met le compteur de bytes à zéro pour la chaine suivante */
+                else if(buff[i]=='\n')  /* si on tombe sur un saut de ligne */
+                {
+                    printf("\nun saut de ligne! compteur de contacts (ctc) = %d\n",ct_contact);
+                    ct_contact+=1; /* incrémente le compteur de contacts */
+                    ct_donnee=0; /* met le compteur de données à zéro pour la suite */
+                    ct_byte=0; /* met le compteur de bytes à zéro pour la chaine suivante */
 
                 }
-                else { /* sinon ça veut dire qu'on lit des données et là c'est à nous d'imaginer
-                            comment les récupérer, il est possible d'utiliser un compteur pour le type
-                            des données, la virgule servant à l'incrémenter, et d'utiliser un tableau
-                            de char temporaire pour sauvegarder la chaine lu à ce moment, puis de
-                            l'enregistrer dans un contact lors du saut de ligne, et le saut de ligne
-                            servira à créer un compteur pour le nombre de contact lu, le compteur du
-                            saut de ligne servira aussi à arrêter la lecture lorsque qu'il atteindra la
-                            capacite de notre fiche */
-                    printf("%c",buff[i]); /* remplacer ce printf par la mise du caractère dans un tableau
-                                             de char utilisant le compteur de bytes pour l'emplacement */
-                    ctb+=1; /* incrémente le compteur de bytes */
+                else
+                {
+                    tampon[ct_byte]=buff[i];
+                    ct_byte+=1; /* incrémente le compteur de bytes */
                 }
             }
         }
