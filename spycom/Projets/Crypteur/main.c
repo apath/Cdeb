fe01c9ab4d57934cd6c4f6273124424e2120355a
cdeb <spycom@orange.fr>
2012-04-16 12:19:52 +0200

    mise a jour crypteur

--- spycom/Projets/Crypteur/main.c
+++ spycom/Projets/Crypteur/main.c
@@ -1,40 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 int main( int argc, char** argv )
 {
-    size_t i,compteur;
-    char lettre;
-    unsigned char buff[1024];
-    FILE *fichier=NULL;
+    int caractere_lu=0,i=0,compteur=0,compteur_point=0;
+    char *key, * lettre, *nom_fichier;
+    char ajout_crypt[11]=".crypt.txt",ajout_supcrypt[5]=".txt",nom_fichier_modif[256];
+    FILE *fichier_decrypte=NULL;
+    FILE *fichier_crypte=NULL;
 
-    printf ("Nombre d'argument : %d\n", argc);
     if(argc==4)
     {
-        lettre = *(argv[1]);
-        if(lettre=='c'){
-            printf("CRYPTEUR:\n");
-            fichier=fopen(argv[2],"rb");
-            if (fichier != NULL){
-                while((compteur=fread(buff,sizeof(char),1024,fichier)))
+        lettre = argv[1];
+        nom_fichier = argv [2];
+        key = argv[3];
+
+        if (key!='\0')
+        {
+            /****************************************cryptage***********************************/
+            if(*lettre=='c')
+            {
+                /**permet de renommer le ficher a crypter avec extension .crypt.txt**/
+                compteur=strlen(nom_fichier);
+                strcpy(nom_fichier_modif,nom_fichier);
+                for (i= compteur; i>0; i--)
                 {
-                    for(i=0;i<compteur;i++){
-                        /* "crypt" uniquement à l'aide du premier char
-                         * du mot de passe */
-                        printf("%c",buff[i]^argv[3][0]);
+                    if (nom_fichier_modif[i] == '.')
+                    {
+                        while(i!=compteur)
+                        {
+                            nom_fichier_modif[i] = '\0';
+                            i++;
+                        }
+                        i=0;
                     }
                 }
-            printf("COUCOU %s",argv[3]);
+                strcat(nom_fichier_modif,ajout_crypt);
+                /*****************************************************/
+                fichier_decrypte=fopen(argv[2],"rb");
+                if (fichier_decrypte != NULL)
+                {
+                    fichier_crypte=fopen(nom_fichier_modif,"wb");
+                    if (fichier_crypte != NULL)
+                    {
+                        while((caractere_lu=getc(fichier_decrypte))!= EOF)
+                        {
+                            if(!*key)key=argv[3];
+                            caractere_lu ^= *(key++);
+                            putc(caractere_lu,fichier_crypte);
+                        }
+                        fclose(fichier_crypte);
+                    }
+                    fclose(fichier_decrypte);
+                }
+            }
+            /****************************************decryptage*********************************/
+            else if(*lettre=='d')
+            {
+                /**permet de renommer le ficher crypter avec extension .txt**/
+                compteur=strlen(nom_fichier);
+                strcpy(nom_fichier_modif,nom_fichier);
+                for (i= compteur; i>0; i--)
+                {
+                    if (nom_fichier_modif[i] == '.')
+                    {
+                        compteur_point++;
+                        if(compteur_point==2)
+                        {
+                            while(i!=compteur)
+                            {
+                                nom_fichier_modif[i] = '\0';
+                                i++;
+                            }
+                            i=0;
+                        }
+                    }
+                }
+                strcat(nom_fichier_modif,ajout_supcrypt);
+               /*****************************************************/
+                fichier_crypte=fopen(argv[2],"rb");
+                if (fichier_crypte != NULL)
+                {
+                    fichier_decrypte=fopen(nom_fichier_modif,"wb");
+                    if (fichier_decrypte != NULL)
+                    {
+                        while((caractere_lu=getc(fichier_crypte))!= EOF)
+                        {
+                            if(!*key)key=argv[3];
+                            caractere_lu ^= *(key++);
+                            putc(caractere_lu,fichier_decrypte);
+                        }
+                        fclose(fichier_decrypte);
+                    }
+                    fclose(fichier_crypte);
+                }
+            }
+            else
+            {
+                printf("Le parametre n'est pas correct.\nLes caracteres 'c' pour crypter et 'd' pour decrypter.\n");
             }
         }
-        else if(lettre=='d'){
-            printf("Decrypteur");
-            printf("Pas coucou %s",argv[3]);
-        }
-        else{
-            printf("Le parametre n'est pas correct. 'c' pour crypter et 'd' pour decrypter");
-        }
+    }
+    else
+    {
+        printf("Le nombre d'argument n'est pas correct.\n");
     }
     return 0;
 }
