efe558acc85befd73336f88bc09601ff5d624e47
cdeb <spycom@orange.fr>
2012-04-22 17:11:13 +0200

    modif cryptage

--- spycom/Projets/Crypteur/main.c
+++ spycom/Projets/Crypteur/main.c
@@ -5,23 +5,10 @@
 
 int main( int argc, char** argv )
 {
-    int caractere_lu=0,i=0,compteur=0,compteur_point=0;
+    int caractere_lu=0,i=0,compteur=0;
     char *key, * lettre, *nom_fichier;
-    char ajout_crypt[11]=".crypt.txt",ajout_supcrypt[5]=".txt",nom_fichier_modif[256];
+    char ajout_crypt[7]=".crypt",nom_fichier_modif[256];
 
-    /* c'est une erreur de créer une extension "à la main" car si on venait à
-     * crypter un fichier .jpg (ton algorithme le permet) ou autre, il se retrouvera avec l'extension
-     * .txt etc,
-     * c'est pourquoi je t'avais conseillé de faire txt.crypt en exemple, d'ajouter le .crypt
-     * à la fin du nom du fichier simplement, et de le retirer lors du decrypt, ça t'aurai
-     * épargné les boucles etc, rendant du même fait ton programme "utilisable pour tout"
-     * suffisait d'un strcat pour l'ajout de .crypt et d'ajouter un \0
-     * placé à la place du . de .crypt pour supprimer le .crypt lors du decryptage.
-     * reduisant le code actuel pour le faire à seulement quelques lignes */
-
-    /* comme tu as pu le remarqué, le cryptage et decryptage avec le xor utilise la même
-     * méthode, il n'y a que l'extension du fichier qui change tu peux alors créer une fonction
-     * qui prend en parametre le nom du fichier à lire, le nom du fichier à créer, et le mot de passe utilisé */
     FILE *fichier_decrypte=NULL;
     FILE *fichier_crypte=NULL;
 
@@ -36,21 +23,8 @@ int main( int argc, char** argv )
             /****************************************cryptage***********************************/
             if(*lettre=='c')
             {
-                /**permet de renommer le ficher a crypter avec extension .crypt.txt**/
-                compteur=strlen(nom_fichier);
+                /**permet de renommer le ficher a crypter avec extension .crypt**/
                 strcpy(nom_fichier_modif,nom_fichier);
-                for (i= compteur; i>0; i--)
-                {
-                    if (nom_fichier_modif[i] == '.')
-                    {
-                        while(i!=compteur)
-                        {
-                            nom_fichier_modif[i] = '\0';
-                            i++;
-                        }
-                        i=0;
-                    }
-                }
                 strcat(nom_fichier_modif,ajout_crypt);
                 /*****************************************************/
                 fichier_decrypte=fopen(argv[2],"rb");
@@ -73,27 +47,22 @@ int main( int argc, char** argv )
             /****************************************decryptage*********************************/
             else if(*lettre=='d')
             {
-                /**permet de renommer le ficher crypter avec extension .txt**/
+                /**permet de renommer le ficher crypter sans l'extension .crypt**/
                 compteur=strlen(nom_fichier);
                 strcpy(nom_fichier_modif,nom_fichier);
                 for (i= compteur; i>0; i--)
                 {
                     if (nom_fichier_modif[i] == '.')
                     {
-                        compteur_point++;
-                        if(compteur_point==2)
+                        while(i!=compteur)
                         {
-                            while(i!=compteur)
-                            {
-                                nom_fichier_modif[i] = '\0';
-                                i++;
-                            }
-                            i=0;
+                            nom_fichier_modif[i] = '\0';
+                            i++;
                         }
+                        i=0;
                     }
                 }
-                strcat(nom_fichier_modif,ajout_supcrypt);
-               /*****************************************************/
+                /*****************************************************/
                 fichier_crypte=fopen(argv[2],"rb");
                 if (fichier_crypte != NULL)
                 {
