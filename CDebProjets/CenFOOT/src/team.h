3f349b21d24d108754a98c8ef1cffcc4890e1e37
apath <ashcroftstraaf@gmail.com>
2012-06-07 17:25:06 +0200

    start

---
+++ CDebProjets/CenFOOT/src/team.h
@@ -0,0 +1,26 @@
+#ifndef __team__
+#define __team__
+/*
+ * cet header est utilisé pour la gestion des équipes
+ */
+
+/* structures temporaire basées sur le cahier des charges et conditions du jeu
+ * necessaire de modifier si besoin avant de se lancer plus loin */
+
+struct team{
+    char *name; /* nom du joueur */
+    int pos,num,def,atk; /* position,numero,points def et atk */
+    struct team *next; /* ptr next (liste chaînée) */
+};
+struct club{
+    char *team_name; /* nom de l'equipe */
+    struct team *tm; /* ptr sur la liste de joueurs dans l'equipe */
+    struct club *next; /* ptr next (liste chaînée) */
+};
+
+/* si structures ok, la suite les fonctions pour les utiliser */
+/* construction et destruction d'une equipe, creation de club
+ * enregistrement dans fichier et chargement pour la sauvegarde des clubs */
+
+
+#endif
