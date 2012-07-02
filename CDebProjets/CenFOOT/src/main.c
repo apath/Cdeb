435535ec5c61595b93df3a009707c3aab1bfcf3d
apath <ashcroftstraaf@gmail.com>
2012-07-02 18:03:58 +0200

    match suite

--- CDebProjets/CenFOOT/src/main.c
+++ CDebProjets/CenFOOT/src/main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "country.h"
+#include "match.h"
+
+/* main de test et debug */
 
 int main(void)
 {
-    printf("alias2code = %d\n",country_alias2code("FR"));
+    /*printf("alias2code = %d\n",country_alias2code("FR"));
     printf("alias2name = %s\n",country_alias2name("FR"));
     printf("name2code  = %d\n",country_name2code("France"));
     printf("name2alias = %s\n",country_name2alias("France"));
     printf("code2name  = %s\n",country_code2name(84));
-    printf("code2alias = %s\n",country_code2alias(84));
+    printf("code2alias = %s\n",country_code2alias(84));*/
+    /* test */
+    MATCH *m;
+    m=malloc(sizeof(MATCH));
+    if(m){
+        m->amical_start=NULL;
+        m->coupe_start=NULL;
+        m->saison_start=NULL;
+    }
+    free(m);
     return 0;
 }
--- CDebProjets/CenFOOT/src/match.h
+++ CDebProjets/CenFOOT/src/match.h
@@ -5,11 +5,11 @@
  * cet header est utilisé pour les matchs
  */
 struct amical{
-
     int score_dom,score_ext;/*Initialisation des scores */
     int remp_dom,remp_ext;/*Initialisation du nombre de remplacant 3 max par equipe*/
     char *team_name_dom;
     char *team_name_ext;
+    struct amical *next; /* pointeur next */
 };
 struct coupe{
     int score_dom,score_ext;/*Initialisation des scores */
@@ -17,6 +17,7 @@ struct coupe{
     int tour[5][16];/* 5 tour de coupe avec 16 rencontre au depart puis 8, 4, 2, 1*/
     char *team_name_dom;
     char *team_name_ext;
+    struct coupe *next; /* pointeur next */
 };
 struct saison{
     int score_dom,score_ext;/*Initialisation des scores */
@@ -24,7 +25,19 @@ struct saison{
     int journee[38][10];/*38 journee avec des 10 rencontres a chaque journee*/
     char *team_name_dom;
     char *team_name_ext;
-    struct *coupe;
+    struct saison *next; /* pointeur next */
+    struct coupe *start; /* < est-ce correct ? ça permet d'incorporer une liste des resultats
+    de coupe dans une saison */
 };
+
+struct match{
+    struct amical *amical_start; /* contiendra l'adr depart pour liste amical */
+    struct coupe *coupe_start; /* contiendra l'adr depart pour liste coupe */
+    struct saison *saison_start; /* contiendra l'adr depart pour liste saison */
+};
+typedef struct match MATCH; /* le type MATCH pour simplifier les fonctions (quand on passe
+                        les pointeurs en paramètre des fonctions là un seul pointeur
+                        type MATCH permet de passer les trois pointeurs et de les modif
+                        à la voler sans avoir à retourner les pointeurs etc */
 
 #endif
