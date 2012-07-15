d469ac33cce600ed82f9911e02acdf83ac0b07dd
apath <ashcroftstraaf@gmail.com>
2012-07-15 13:57:06 +0200

    debug

--- CDebProjets/CenFOOT/compile_d_e.bat
+++ CDebProjets/CenFOOT/compile_d_e.bat
@@ -1,4 +1,4 @@
-c:\mingw\bin\gcc -ansi -Wall -Wextra src\main.c src\country.c -o bin\debug\main.exe
+c:\mingw\bin\gcc -ansi -Wall -Wextra src\main.c src\country.c src\calend.c -o bin\debug\main.exe
 @pause
 c:\mingw\bin\gdb bin\debug\main.exe
 
